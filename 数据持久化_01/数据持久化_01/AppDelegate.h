190f30852c9ad55cde6c1f373fabaf2a89fcb2d5
apple <apple@appledemacbook-pro.local>
2016-08-30 14:11:53 +0800

    获取app目录

---
+++ 数据持久化_01/数据持久化_01/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  数据持久化_01
+//
+//  Created by apple on 16/8/30.
+//  Copyright © 2016年 Lingser. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 数据持久化_01/数据持久化_01/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  数据持久化_01
+//
+//  Created by apple on 16/8/30.
+//  Copyright © 2016年 Lingser. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
